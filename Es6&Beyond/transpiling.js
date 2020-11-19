c27080f8f88fb254007e2907d79948c7c00d75c1
maxyee <eyaminkhan00@gmail.com>
2020-11-20 01:45:51 +0600

    Es6 and beyond transpiling

---
+++ Es6&Beyond/transpiling.js
@@ -0,0 +1,11 @@
+var foo = [1,2,3]
+
+var obj = {
+    // foo : foo
+    foo   // means foo: foo
+    
+    // transpiling helping to shorten the code 
+    // if the names are same   
+}
+
+obj.foo

85fcb6600eee65b528719ec6a6239b2ad8f8d807
maxyee <eyaminkhan00@gmail.com>
2021-01-23 00:09:57 +0600

    wrote method and value expression file added

--- Es6&Beyond/default-parameter-values.js
+++ Es6&Beyond/default-parameter-values.js
@@ -26,4 +26,25 @@ foo( undefined, 6); // 17
 function foo(x,y){
     x = (0 in arguments ) ? x : 11;
     y = (1 in arguments ) ? y : 31;
+
+    console.log( x + y )
 }
+
+foo(5);                 // 36
+foo(5, undefined);      // NaN
+
+//----------------------------------
+function foo(x = 11, y = 31){
+    console.log( x + y )
+}
+
+foo();          //42
+foo(5, 6);      //11
+foo(0, 42);     //42
+
+foo( 5 );       //36
+foo(5, undefined);  // 36 <---- `undefined is missing`
+foo(5, null);   //5 <---- null coerces to '0'
+
+foo(undefined, 6);  //17 <----- `undefined` is missing
+foo(null, 6);       //6 <------ null coerces to '0'

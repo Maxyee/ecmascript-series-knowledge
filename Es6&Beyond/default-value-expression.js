767512dfed00caa149836568a429a741427e3860
maxyee <eyaminkhan00@gmail.com>
2021-01-27 23:26:57 +0600

    default value expression examples

--- Es6&Beyond/default-value-expression.js
+++ Es6&Beyond/default-value-expression.js
@@ -14,3 +14,33 @@ foo(10);        // "bar called"
                 // 10 15
 y = 6;
 foo(undefined, 10);    //9 10
+
+// -----------------------------------
+
+var w = 1, z = 2;
+
+function foo(x = w + 1, y = x + 1, z = z + 1){
+    console.log(x, y, z)
+}
+
+foo();    // reference error
+
+
+// ----------------------------------
+
+function foo( x = 
+        (function(v){ return v + 11; })(31)
+    ){
+        console.log(x)
+}
+
+foo();      // 42
+
+
+// ----------------------------------
+
+function ajax(url, cb = function(){}){
+    //....
+}
+
+ajax("http://some.url.1");

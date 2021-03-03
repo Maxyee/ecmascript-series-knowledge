233a9d7b01a01f1888a81a85cc52b2b1e52999e3
maxyee <eyaminkhan00@gmail.com>
2021-03-04 04:52:37 +0600

    nested destructuring

--- Es6&Beyond/default-value-assignment.js
+++ Es6&Beyond/default-value-assignment.js
@@ -15,3 +15,17 @@ var {x = 5, y = 10, z = 15, w = 20 } = bar();
 
 console.log(a, b, c, d);       // 1 2 3 12
 console.log(x, y, z, w);       // 4 5 6 20
+
+// -------------------------------
+var { x, y, z, w: WW = 20 } = bar();
+console.log( x, y, z, WW );     // 4 5 6 20
+
+//--------------------------------
+var x = 200 , y = 300, z = 100;
+var o1 = { x: { y: 42 }, z: {y: z } };
+
+( {y: x = {y: y}} = o1 );
+( {z: y = {y: z}} = o1 );
+( {x: z = {y: x}} = o1 );
+
+console.log( x.y, y.y, z.y );   // 300 100 42
---
+++ Es6&Beyond/nested-destructuring.js
@@ -0,0 +1,21 @@
+var a1 = [ 1, [2, 3, 4], 5 ];
+var o1 = { x: {y: {z: 6} } };
+
+var [a, [b, c, d], e ] = a1;
+var {x: { y: { z: w } } } = o1;
+
+console.log(a, b, c, d, e);    // 1 2 3 4 5
+console.log( w );              // 6
+
+// ---------------------------------------------
+
+var app = {
+    model: {
+        User: function(){  }
+    }
+};
+
+// instead of:
+// var User = App.model.User
+
+var { model: { User } } = App;

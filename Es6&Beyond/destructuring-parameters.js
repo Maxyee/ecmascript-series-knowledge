1f73aee93ac5436dea88ad998bce1a0bdc3ea3e4
maxyee <eyaminkhan00@gmail.com>
2021-03-08 23:56:58 +0600

    quick illustration code snippet

--- Es6&Beyond/destructuring-parameters.js
+++ Es6&Beyond/destructuring-parameters.js
@@ -25,3 +25,19 @@ foo({ y: 42 });         // undefined 42
 foo( {} );              // undefined undefined
 
 //--------------------------------------
+
+function f1([x=2, y=3, z]){/*..*/}
+function f2([x, y, ...z ], w) {/*..*/}
+function f3([x, y, ...z ], ...w){/*..*/}
+
+function f4({x: X, y}) {/*..*/}
+function f5({x: X = 10, y = 20 }){/*..*/}
+function f6({x = 10} = {}, { y } = { y:10 }){ /*..*/ }
+
+function f3([x, y, ...z], ...w){
+    console.log(x, y, z, w);
+}
+
+f3([]);                 // undefined undefined [] []
+f3([1,2,3,4],5, 6);     // 1 2 [3,4] [5,6]
+

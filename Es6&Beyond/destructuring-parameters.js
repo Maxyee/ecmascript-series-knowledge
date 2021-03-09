7b385b41374e90c621371cad8bfca08fbe6da26a
maxyee <eyaminkhan00@gmail.com>
2021-03-09 23:50:47 +0600

    Destructuring and parameter defaults

--- Es6&Beyond/destructuring-parameters.js
+++ Es6&Beyond/destructuring-parameters.js
@@ -24,7 +24,7 @@ foo({ y: 1, x: 2 } );   // 2 1
 foo({ y: 42 });         // undefined 42
 foo( {} );              // undefined undefined
 
-//--------------------------------------
+//----------------------------------------------
 
 function f1([x=2, y=3, z]){/*..*/}
 function f2([x, y, ...z ], w) {/*..*/}
@@ -41,3 +41,20 @@ function f3([x, y, ...z], ...w){
 f3([]);                 // undefined undefined [] []
 f3([1,2,3,4],5, 6);     // 1 2 [3,4] [5,6]
 
+//-----------------------------------------------
+
+// Destructuring Defaults + Parameter Defaults
+
+function f6({ x = 10} = {}, { y } = { y: 10}){
+    console.log( x, y );
+}
+
+f6();           // 10 10
+f6( undefined, undefined ); // 10 10
+f6( {}, undefined );    // 10 10
+f6( {}, {} );    // 10 undefined
+f6( undefined, {} ) // 10 undefined
+f6( {x: 2}, { y: 3} )   // 2 3
+
+
+// Nested Defaults: Destructuring and Restructured
